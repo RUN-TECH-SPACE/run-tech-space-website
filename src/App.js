96c51ee939c3f59ea7cafe4631642b063d4e4f31
jomefavourite <jfjomefavourite@gmail.com>
2022-05-09 12:35:08 +0100

    setup project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
