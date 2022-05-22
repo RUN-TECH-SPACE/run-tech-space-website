f07cf0129bb36174df2ce9338301b701e65b85fb
jomefavourite <jfjomefavourite@gmail.com>
2022-05-22 07:51:14 +0100

    changed env name

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 import Airtable from "airtable";
 
 export const base = new Airtable({
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-}).base(process.env.NEXT_PUBLIC_BASE);
+  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_KEY,
+}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE);
