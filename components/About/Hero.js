092f681579aa7f1083b2bd1972bf7194f4cdf9b2
jomefavourite <jfjomefavourite@gmail.com>
2022-05-30 11:13:36 +0100

    about image

--- components/About/Hero.js
+++ components/About/Hero.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import React from "react";
+import  AboutImage  from "../../public/images/about-image.png"
 
 function Hero() {
   return (
@@ -6,7 +8,7 @@ function Hero() {
       <div className='order-2 space-y-3 self-center md:order-1'>
         <h2 className='heading2 text-left'>What really is RUN Tech Space?</h2>
         <p className=' md:text-xl'>
-          Run Tech Space, a short form for Redeemer’s University Technology
+          Run Tech Space, a short form for Redeemer's University Technology
           Space, is a creative environment for students to learn, brainstorm and
           innovate solutions to societal challenges.
         </p>
@@ -18,11 +20,15 @@ function Hero() {
         </p>
       </div>
 
-      <img
+      {/* <AboutImage /> */}
+
+      <Image src={AboutImage} objectFit className="order-1 justify-self-center md:order-2 md:justify-self-end" prioritys />
+
+      {/* <img
         src='/images/heroBg.png'
         alt='banner'
         className='order-1 justify-self-center md:order-2 md:justify-self-end'
-      />
+      /> */}
     </header>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -16,11 +16,11 @@ function About() {
   ];
 
   const run = [
-    "1.	RUN tech space will boost the image of Redeemer’s University by competing in Tech competition both locally and internationally.",
-    "2.	RUN tech space will help Redeemer’s university, Nigeria and probably the world with her projects and help bring solutions to some of her problems.",
-    "3.	RUN tech space will portray Redeemer’s university as one of the universities at the frontline of technological advancement in Nigeria.",
-    "4.	RUN tech space will portray Redeemer’s university as the home of creativity and innovation in Africa.",
-    "5.	RUN tech space will help Redeemer’s university students get grants and awards.",
+    "1.	RUN tech space will boost the image of Redeemer's University by competing in Tech competition both locally and internationally.",
+    "2.	RUN tech space will help Redeemer's university, Nigeria and probably the world with her projects and help bring solutions to some of her problems.",
+    "3.	RUN tech space will portray Redeemer's university as one of the universities at the frontline of technological advancement in Nigeria.",
+    "4.	RUN tech space will portray Redeemer's university as the home of creativity and innovation in Africa.",
+    "5.	RUN tech space will help Redeemer's university students get grants and awards.",
   ];
 
   return (
